190e28fd04d124e19d0b4fd0d9f4cbdd3b837701
enrique r grullon <co8@users.noreply.github.com>
2022-11-28 11:43:56 -0500

    refactor sheets API

--- libs/sheets.js
+++ libs/sheets.js
@@ -1,15 +1,18 @@
 import { google } from "googleapis";
 export async function getBRVotesList() {
   try {
-    const auth = await google.auth.getClient({
-      scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"],
-    });
+    const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+    const jwt = new google.auth.JWT(
+      process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+      null,
+      (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+      target
+    );
 
-    const sheets = google.sheets({ version: "v4", auth });
-
+    const sheets = google.sheets({ version: "v4", auth: jwt });
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SPREADSHEET_ID,
-      range: "brvotes", //sheet name
+      range: process.env.SPREADSHEET_NAME,
     });
 
     const rows = response.data.values;
