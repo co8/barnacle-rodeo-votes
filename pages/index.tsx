3f7626b9f8d8fb51c227c12482a0b398cb40b4e7
enrique r grullon <co8@users.noreply.github.com>
2022-12-07 21:43:04 -0500

    adding lightbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,27 @@
+import { useState } from "react";
 import Head from "next/head";
 import { getBRVotesList } from "../libs/sheets";
 import { Gallery } from "react-grid-gallery";
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 import HeaderComponent from "../components/header";
 
 export default function IndexPage({ votes_api }: { votes_api: object }) {
+  const [index, setIndex] = useState(-1);
+
+  const images = Object.values(votes_api);
+
+  const currentImage = images[index];
+  const nextIndex = (index + 1) % images.length;
+  const nextImage = images[nextIndex] || currentImage;
+  const prevIndex = (index + images.length - 1) % images.length;
+  const prevImage = images[prevIndex] || currentImage;
+
+  const handleClick = (index: number, item: CustomImage) => setIndex(index);
+  const handleClose = () => setIndex(-1);
+  const handleMovePrev = () => setIndex(prevIndex);
+  const handleMoveNext = () => setIndex(nextIndex);
+
   return (
     <>
       <Head>
@@ -25,7 +43,27 @@ export default function IndexPage({ votes_api }: { votes_api: object }) {
       </Head>
       <HeaderComponent />
       <div id="media">
-        <Gallery images={Object.values(votes_api)} />
+        <Gallery
+          images={images}
+          onClick={handleClick}
+          enableImageSelection={false}
+        />
+        {!!currentImage && (
+          /* @ts-ignore */
+          <Lightbox
+            enableZoom={false}
+            mainSrc={currentImage.src}
+            imageTitle={currentImage.caption}
+            //mainSrcThumbnail={currentImage.src}
+            nextSrc={nextImage.src}
+            //nextSrcThumbnail={nextImage.src}
+            prevSrc={prevImage.src}
+            //prevSrcThumbnail={prevImage.src}
+            onCloseRequest={handleClose}
+            onMovePrevRequest={handleMovePrev}
+            onMoveNextRequest={handleMoveNext}
+          />
+        )}
       </div>
     </>
   );
