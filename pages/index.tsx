65c3b37da312e1c838aafd59570f34a41e65a078
enrique r grullon <co8@users.noreply.github.com>
2022-12-12 18:55:24 -0500

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,6 +45,13 @@ export default function IndexPage({ votes_api }: { votes_api: object }) {
           content="https://votes.barnacle.rodeo/images/89.jpg"
         />
         <meta property="og:url" content="https://barnacle.rodeo/votes" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <HeaderComponent />
       <div id="media">
